953d96636e5806852576a35d2c3c134af30a9476
nicolascwy <nicolas.chang99@gmail.com>
2022-12-05 22:26:51 +0800

    Amend day 1 solution
    Amend solution to read solution from file of piping it

--- day-1-p1.cpp
+++ day-1-p1.cpp
@@ -1,43 +1,47 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <fstream>
 
 const std::string EMPTY_LINE = "\n";
 
-int processCal(int cal, int maxCal)
+int main(int argc, char *argv[])
 {
-  if (maxCal < cal)
-  {
-      return cal;
-  }
-
-  return maxCal;
-};
-
-int main()
-{
+  std::ifstream file;
   std::string lineData;
   int totalCalCount = 0;
   int maxCal = 0;
-  while (getline(std::cin, lineData))
+
+  if (argc < 2)
   {
-    // Reset var for new elf and check if new maximum calories found
-    if (lineData.length() == 0)
-    {
-      // Read second new line char in empty line
-      getline(std::cin, lineData);
-
-      maxCal = processCal(totalCalCount, maxCal);
-      totalCalCount = 0;
-    } 
-
-    int cal;
-    std::stringstream(lineData) >> cal;
-    totalCalCount += cal;
+    std::cout << "Insufficient arguments, provide the file name";
+    return -1;
   }
 
-  // For last elf - since file does not end with \n\n
-  maxCal = processCal(totalCalCount, maxCal);
+  file.open(argv[1]);
+  
+  if (file.is_open())
+  {
+    while (getline(file, lineData))
+      {
+        // Reset var for new elf and check if new maximum calories found
+        if (lineData.length() == 0)
+        {
+          // Read second new line char in empty line
+          getline(file, lineData);
 
-  std::cout << maxCal;
+          maxCal = totalCalCount > maxCal ? totalCalCount : maxCal;
+          totalCalCount = 0;
+        } 
+
+        int cal;
+        std::stringstream(lineData) >> cal;
+        totalCalCount += cal;
+      }
+
+      std::cout << maxCal;
+  } else {
+    std::cout << "Unable to open file";
+  }
+  
 }
--- day-1-p2.cpp
+++ day-1-p2.cpp
@@ -3,54 +3,60 @@
 #include <queue>
 #include <vector>
 #include <sstream>
+#include <fstream>
 
 const std::string EMPTY_LINE = "\n";
 
-void processCal(int cal, std::priority_queue<int, std::vector<int>, std::greater<int>> &pq)
+int main(int argc, char *argv[])
 {
-    if (pq.size() < 3)
-      {
-        pq.push(cal);
-        std::cout << "Adding " << cal << " to the heap\n";
-      } else if (pq.top() < cal) {
-        std::cout << "Removing " << pq.top() << " and adding " << cal << " to the heap\n";
-        pq.pop();
-        pq.push(cal);
-      }
-      
-};
-
-int main()
-{
+  std::ifstream file;
   std::string lineData;
   int totalCalCount = 0;
   std::priority_queue<int, std::vector<int>, std::greater<int>> topCalories = {};
   int numElInHeap = 0;
 
-  while (getline(std::cin, lineData))
+  if (argc < 2)
   {
-    // Reset var for new elf and check if new maximum calories found
-    if (lineData.length() == 0)
-    {
-      // Read second new line char in empty line
-      // line contains 2 new lines, getline reads till the first \n
-      getline(std::cin, lineData);
-      processCal(totalCalCount, topCalories);
-      totalCalCount = 0;
-    } 
-
-    int cal;
-    std::stringstream(lineData) >> cal;
-    totalCalCount += cal;
+    std::cout << "Insufficient arguments, provide the file name";
+    return -1;
   }
-  // For last elf - since file does not end with \n\n
-  processCal(totalCalCount, topCalories);
 
-  int sum = 0; 
-  while (!topCalories.empty())
+  file.open(argv[1]);
+  if (file.is_open())
   {
-    sum += topCalories.top();
-    topCalories.pop();
+    while (getline(file, lineData))
+      {
+        // Reset var for new elf and check if new maximum calories found
+        if (lineData.length() == 0)
+        {
+          // Read second new line char in empty line
+          // line contains 2 new lines, getline reads till the first \n
+          getline(std::cin, lineData);
+          if (topCalories.size() < 3)
+          {
+            topCalories.push(totalCalCount);
+            std::cout << "Adding " << totalCalCount << " to the heap\n";
+          } else if (topCalories.top() < totalCalCount) {
+            std::cout << "Removing " << topCalories.top() << " and adding " << totalCalCount << " to the heap\n";
+            topCalories.pop();
+            topCalories.push(totalCalCount);
+          }
+          totalCalCount = 0;
+        } 
+
+        int cal;
+        std::stringstream(lineData) >> cal;
+        totalCalCount += cal;
+      }
+
+      int sum = 0; 
+      while (!topCalories.empty())
+      {
+        sum += topCalories.top();
+        topCalories.pop();
+      }
+      std::cout << sum;
+  } else {
+    std::cout << "Unable to open file";
   }
-  std::cout << sum;
 }
