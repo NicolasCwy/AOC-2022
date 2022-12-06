9333076f1c80ef3298c3e2df26ba2b73b6737671
nicolascwy <nicolas.chang99@gmail.com>
2022-12-06 12:31:24 +0800

    Add Day 2 solutions

---
+++ day-2-p2.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <utility>
+#include <vector>
+#include <sstream>
+#include <fstream>
+
+std::unordered_map<std::string, std::string> weakness = {{"scissors", "rock"},
+  {"rock", "paper"},
+  {"paper", "scissors"}};
+
+std::unordered_map<std::string, std::string> strength = {{"rock", "scissors"},
+  {"paper", "rock"},
+  {"scissors", "paper"}};
+
+std::unordered_map<std::string, std::string> inputToSymbol= {{"A", "rock"},
+  {"B", "paper"},
+  {"C", "scissors"},
+  {"X", "rock"},
+  {"Y", "paper"},
+  {"Z", "scissors"}};
+
+std::unordered_map<std::string, int> symbolScore = {{"rock", 1},
+    {"paper", 2},
+    {"scissors", 3}};
+
+// Returns true if x is y's weakness
+bool xBeatsY(std::string x, std::string y) {
+  return weakness.at(y) == x;
+}
+
+std::vector<std::pair<std::string, std::string>> getInput(std::string filename) {
+  std::fstream file;
+  std::string line;
+  std::vector<std::pair<std::string, std::string>> input;
+
+  file.open(filename);
+  if (file.is_open())
+  {
+    while (getline(file, line))
+    {
+      // split line into 2 characters
+      // first character is opponent
+      // second char is me
+      std::stringstream ss(line);
+      std::string colOne;
+      std::string colTwo;
+
+      ss >> colOne;
+      ss >> colTwo;
+
+      std::pair<std::string, std::string> pair = std::make_pair(inputToSymbol.at(colOne), colTwo);
+      input.emplace_back(pair);
+    }
+
+  } else {
+    std::cout << "Unable to open file";
+  }
+
+  return input;
+}
+
+int main(int argc, char *argv[])
+{
+  int score = 0;
+
+  if (argc < 2)
+  {
+    std::cout << "Insufficient arguments, provide the file name";
+    return -1;
+  }
+
+  std::vector<std::pair<std::string, std::string>> input = getInput(argv[1]);
+  // iterate through input
+  for (std::vector<std::pair<std::string, std::string>>::iterator it = input.begin(); it != input.end(); ++it)
+  {
+    std::string oppSymbol = std::get<0>(*it);
+    std::string outcome = std::get<1>(*it);
+    std::string mySymbol = "";
+
+    // Score from comparing choice with opponents
+    // 1. beat opponent (col 2 beat col 1) 
+    if (outcome == "Z") {
+      score += 6;
+      mySymbol = weakness.at(oppSymbol);
+    // 2. beaten by opponent (col 1 beat col 2)
+    } else if (outcome == "X") {
+      mySymbol = strength.at(oppSymbol);
+    // 3. Neither beat each other (Draw)
+    } else {
+      score += 3;
+      mySymbol = oppSymbol;
+    }
+    // Score from choosing a symbol
+    score += symbolScore.at(mySymbol);
+  }
+
+  std::cout << score;
+}
